a0a3504c9d5345f64b2365701c0873da1dd722de
qianghe <qianghe421@163.com>
2017-04-15 19:47:10 +0800

    fixed  #5

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {NavController, ModalController} from "ionic-angular";
+import {AlertController, NavController, ModalController} from "ionic-angular";
 import {ProjectPage} from "../project/project";
 import {WelcomePage} from "../welcome/welcome";
 import {ApiService} from "../../providers/api.service";
@@ -22,7 +22,7 @@ export class HomePage {
 
     constructor(public navCtrl: NavController, private apiService: ApiService,
                 private authService: AuthService,
-                public modalCtrl: ModalController) {
+                public modalCtrl: ModalController, private alertCtrl: AlertController) {
         this.userInfo = this.authService.userInfo;
 
         this.pages = [
@@ -47,9 +47,26 @@ export class HomePage {
 
     //退出
     logout() {
-        this.navCtrl.setRoot(WelcomePage);
-        localStorage.removeItem('Authorization');
-        this.apiService.createAuthorizationHeader();
+        let confirm = this.alertCtrl.create({
+            title: '确定注销?',
+            buttons: [
+                {
+                    text: '取消',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: '确定',
+                    handler: () => {
+                        this.navCtrl.setRoot(WelcomePage);
+                        localStorage.removeItem('Authorization');
+                        this.apiService.createAuthorizationHeader();
+                    }
+                }
+            ]
+        });
+        confirm.present();
     }
 
     presentModal() {
