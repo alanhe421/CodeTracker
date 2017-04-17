a755859c90d59558d754293218d229e15797cac1
qianghe <qianghe421@163.com>
2017-04-17 20:25:09 +0800

    fixed #9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AuthApp {
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
-            this.statusBar.styleDefault();
+            this.statusBar.styleLightContent();
             this.splashScreen.hide();
             if (this.authService.isLoggedIn) {
                 this.rootPage = HomePage;
