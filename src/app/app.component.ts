cb9f51f30cbc602af0e7138805c9b495d2d09971
qianghe <qianghe421@163.com>
2017-04-22 23:24:16 +0800

    fixed #8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,13 @@ import {SplashScreen} from "@ionic-native/splash-screen";
 import {StatusBar} from "@ionic-native/status-bar";
 import {AuthService} from "../providers/auth.service";
 import {HomePage} from "../pages/home/home";
-import {Page} from "ionic-angular/umd/navigation/nav-util";
 
 
 @Component({
     templateUrl: 'app.html'
 })
 export class AuthApp {
-    rootPage: Page = WelcomePage;
+    rootPage: any = WelcomePage;
 
     constructor(platform: Platform, public splashScreen: SplashScreen, public statusBar: StatusBar, private authService: AuthService) {
         platform.ready().then(() => {
