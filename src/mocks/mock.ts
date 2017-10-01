b22d9a6f09036dc3346094c352f31dbd8c2d45c3
alanhg <qianghe421@163.com>
2017-10-01 10:40:36 +0800

    update mock-use

--- src/mocks/mock.ts
+++ src/mocks/mock.ts
@@ -2,16 +2,20 @@
  * Created by He on 5/30/17.
  * mock and use native plugins in the browser through a simple override mechanic
  */
-export class AppVersionMock {
-    version = '1.0.0';
+import {AppVersion} from "@ionic-native/app-version";
+import {Device} from "@ionic-native/device";
+
+export class AppVersionMock extends AppVersion {
+    version = "1.0.0";
 
     public getVersionNumber() {
         return new Promise((resolve, reject) => {
-            resolve(this.version)
+            resolve(this.version);
         });
     }
 }
-export class DeviceMock {
+
+export class DeviceMock extends Device {
     platform: string = 'Android';//iOS,Android
     uuid: string = "ae932e768a1a848";
     version: string = "6.0";
